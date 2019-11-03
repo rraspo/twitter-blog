9afc658463aea6abf5f574e9ec04e36bb755cdb1
Juan A. Porras <juanabrahamporras@gmail.com>
2019-11-02 22:39:48 -0600

    Added jumbotron to show user entry count and their username. Sent authenticated user to react component.

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -3,6 +3,20 @@
 @section('content')
 <div class="container">
     <div class="row">
+        <div class="col-md-12">
+            <div class="jumbotron">
+                <h5>Welcome to {{ $user->username }}'s profile</h5>
+                <div class="row">
+                    <div class="col-md-6">
+                        {{ $entries->count() }} {{ __('total') }} {{ Str::plural('entry', $entries->count()) }}
+                    </div>
+                    <div class="col-md-6">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-body">
@@ -23,7 +37,7 @@
         </div>
         <div class="col-md-4">
             {{-- React component to handle Twitter API, see `resources/js/` --}}
-            <div id="twitter-timeline" data-user="{{ $user->toJson() }}"></div>
+            <div id="twitter-timeline" data-user="{{ $user->toJson() }}" data-autheduser="{{ Auth::user()->id ?? '' }}"></div>
         </div>
     </div>
 </div>
